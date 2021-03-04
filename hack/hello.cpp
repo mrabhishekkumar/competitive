dcd514e47846f9e7d87f0ca6968eab5079ce69d6
mrabhishekkumar <rishuzb945@gmail.com>
2021-03-05 02:43:24 +0530

    intil_push

---
+++ hack/hello.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+void rishu(){
+    
+
+
+
+}
+
+int32_t main(){
+    int t=1;
+    // cin>>t;
+    
+    while(t--)rishu();
+
+    return 0;
+}
